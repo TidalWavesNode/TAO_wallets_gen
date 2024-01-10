e6c152ccfe07b75b826c659034d8424907ee2f1f
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2024-01-10 09:22:29 -0600

    Update wallets.sh

--- wallets.sh
+++ wallets.sh
@@ -11,31 +11,15 @@ create_hot_wallets() {
     read -p "Enter the number of hot wallets to create: " num_hot_wallets
 
     for ((i=1; i<=$num_hot_wallets; i++)); do
-        HName="hotwallet_$i"
-        btcli w new_hotkey --wallet.name $COLD --wallet.hotkey $HName --subtensor.network finney --no_prompt
+        HName="$i"
+        btcli w new_hotkey --wallet.name $COLD --wallet.hotkey $i --subtensor.network finney --no_prompt
     done
 }
 
 # Function to show wallet list
 show_wallet_list() {
-    echo "Current Wallets:"
+    echo "Show Created Wallets:"
     btcli w list
-}
-
-# Function to register hot wallets
-register_hot_wallets() {
-    read -p "Do you want to start the registration script? (yes/no): " start_registration
-    if [ "$start_registration" == "yes" ]; then
-        read -p "Enter netuid: " SN
-
-        for ((i=1; i<=$num_hot_wallets; i++)); do
-            HName="$i"
-            btcli s register --netuid $SN --wallet.name $COLD --wallet.hotkey $HName --subtensor.network finney --no_prompt
-            sleep 10
-        done
-    else
-        echo "Registration script not started."
-    fi
 }
 
 # Redirect output to WALLETBACKUPS file
@@ -52,7 +36,4 @@ create_hot_wallets
 # Show wallet list
 show_wallet_list
 
-# Register hot wallets
-register_hot_wallets
-
-echo "Cold wallet and hot wallets created successfully."
+echo "Cold and hot wallets created successfully."
